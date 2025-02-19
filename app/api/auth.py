676b6be48fa4287d4b63d33ddc19c4b0d0367736
Abramov Danil <abramovd200@gmail.com>
2025-02-19 22:52:47 +0300

    update auth: add refresh

--- app/api/auth.py
+++ app/api/auth.py
@@ -1,9 +1,10 @@
-from fastapi import APIRouter, Depends, HTTPException,Response
+from fastapi import APIRouter, Depends, HTTPException, Response, Request
 from sqlalchemy.orm import Session
 from app.dependency.database import get_db
 from app.dependency.auth import get_password_hash, verify_password
 from app.models.models import User, Chat, UserChat, Message
 from sqlalchemy.future import select
+from app.core.security import create_refresh_token, create_access_token, decode_token
 from app.schemas.auth import (
     CreateUserSchema,
     UserResponse,
@@ -36,50 +37,48 @@ async def createuser(user: CreateUserSchema, db: Session = Depends(get_db)):
     return {"id": new_user.id, "email": new_user.email, "name": new_user.username}
 
 
-@router.post("/auth/login", response_model=LoginResponseSchema)
+@router.post("/login", response_model=LoginResponseSchema)
 async def login(user: LoginUserSchema, response: Response, db: Session = Depends(get_db)):
     stmt = select(User).where(User.email == user.email)
     result = await db.execute(stmt)
-    existing_user = result.scalars().first()
-    if existing_user:
-        if verify_password(get_password_hash(existing_user.password), user.password):
-            access_token = security.create_access_token(uid=str(existing_user.id))
-            my_refresh_token = security.create_refresh_token(uid=str(existing_user.id))
-            response.set_cookie(config.JWT_ACCESS_COOKIE_NAME, access_token)
-            response.set_cookie(config.JWT_REFRESH_COOKIE_NAME, my_refresh_token)
+    current_user = result.scalars().first()
+    if current_user:
+        if verify_password(user.password, current_user.password):
+            access_token = create_access_token(
+                data={"sub": current_user.id}
+            )
+            refresh_token = create_refresh_token(
+                data={"sub": current_user.id}
+            )
+            response.set_cookie("access_token", access_token)
+            response.set_cookie("refresh_token", refresh_token)
             return {"message": "Logged in successfully",
                     "access_token": access_token,
-                    "refresh_token": my_refresh_token}
+                    "refresh_token": refresh_token}
         else:
             raise HTTPException(status_code=401, detail="Unauthorized")
 
+@router.post("/refresh", response_model=RefreshTokenResponseSchema)
+async def refresh_token(response: Response, request: Request, db: Session = Depends(get_db)):
+    refresh_token = request.cookies.get("refresh_token")
+    if not refresh_token:
+        raise HTTPException(status_code=401, detail="Refresh token not found")
+    payload = decode_token(refresh_token)
+    try:
+        user_id = payload.sub
+        stmt = select(User).where(User.email == user_id)
+        result = await db.execute(stmt)
+        current_user = result.scalars().first()
+        if not current_user:
+            raise HTTPException(status_code=401, detail="User not found")
+        access_token = create_access_token(
+            data={"sub": current_user.id}
+        )
+        return {"message": "Access token refreshed", "access_token": access_token}
+    except Exception as e:
+        raise HTTPException(status_code=401, detail="Invalid or expired refresh token")
 
-# @router.post("/auth/refresh", response_model=RefreshTokenResponseSchema)
-# async def refresh_token(response: Response, request: Request, db: Session = Depends(get_db)):
-#     refresh_token = request.cookies.get(config.JWT_REFRESH_COOKIE_NAME)
-#     if not refresh_token:
-#         raise HTTPException(status_code=401, detail="Refresh token not found")
-#     payload = security._decode_token(refresh_token)
-#     try:
-#         user_id = payload.sub
-#         existing_user = db.query(User).filter(User.id == int(user_id)).first()
-#         if not existing_user:
-#             raise HTTPException(status_code=401, detail="User not found")
-#         access_token = security.create_access_token(uid=str(existing_user.id))
-#         response.set_cookie(config.JWT_ACCESS_COOKIE_NAME, access_token)
-#         return {"message": "Access token refreshed", "access_token": access_token}
-#     except Exception as e:
-#         raise HTTPException(status_code=401, detail="Invalid or expired refresh token")
-#     #     response.set_cookie(
-#     #         key="access_token",
-#     #         value=access_token,
-#     #         httponly=True,
-#     #         secure=True,
-#     #         samesite="Lax",
-#     #         max_age=3600  # 1 час
-#     #     )
-#
-#
+
 # @router.post("/auth/logout", response_model=LogoutResponseSchema)
 # async def logout(response: Response, request: Request, db: Session = Depends(get_db)):
 #     my_refresh_token = request.cookies.get(config.JWT_REFRESH_COOKIE_NAME)
--- app/core/security.py
+++ app/core/security.py
@@ -1,7 +1,48 @@
 import jwt
 from jwt.exceptions import InvalidTokenError
+from datetime import datetime, timedelta, timezone
+from jose import JWTError, jwt
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 10
-REFRESH_TOKEN_EXPIRE_MINUTES = 1440  # 1 day
+REFRESH_TOKEN_EXPIRE_DAYS = 7
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    """
+    :param data:
+    :param expires_delta:
+    :return: access_token
+    """
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=30)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def create_refresh_token(data: dict, expires_delta: timedelta | None = None):
+    """
+    :param data:
+    :param expires_delta:
+    :return: refresh_token
+    """
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(days=7)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def decode_token(token: str):
+    """
+    :param token:
+    :return:
+    """
+    try:
+        return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    except JWTError:
+        return None
